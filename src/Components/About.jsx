e6650044ba64a69f39d76cfe3dab729d9c3198ac
Telcia <sybelhele@gmail.com>
2023-11-02 22:19:50 -0700

    changes to portofolio

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,20 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a Electronic and Computer Enginner student at Pedagogical University of Maputo. I enjoy creating websites and electronic projects.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
+  "Web development",
   "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Public comunication",
+  "Arduino Programing",
+  "Project Managment",
+  "Team working",
+  
 ];
 
 /**
@@ -45,7 +46,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation. By web development and electronic, I continually look for new and better ways to make tech accessible by all.";
 
 const About = () => {
   return (
